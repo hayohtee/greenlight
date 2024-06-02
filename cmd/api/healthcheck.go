f0b6bd56c52a354d8ec60bcc9b407080dde2d448
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-02 16:36:20 +0100

    Update healthcheckHandler to use json.Marshal

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
-// healthcheckHandler write application information as a fixed-format json response
+// healthcheckHandler write application information as json response
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
-
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	js = append(js, '\n')
 	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write([]byte(js))
+	_, _ = w.Write(js)
 }
