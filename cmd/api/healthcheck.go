2ae5ba85fca134bd023350e5351e7516f273b5ac
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-02 16:46:48 +0100

    Update healthcheckHandler to use writeJSON helper method

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -12,13 +11,9 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"environment": app.config.env,
 		"version":     version,
 	}
-	js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Print(err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
 	}
-	js = append(js, '\n')
-	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write(js)
 }
