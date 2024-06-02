18721ca743a50c46132d8b0b70158554f45f5f2f
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-02 16:07:55 +0100

    Add comments to readIDParam helper method

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 )
 
+// readIDParam retrieve the "id" URL parameter from the current context, then
+// convert it to an integer and return it.
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
