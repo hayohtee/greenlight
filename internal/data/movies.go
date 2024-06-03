fa7c8b7b6b9a8caca2155a897466a54fda63cee0
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-03 11:57:52 +0100

    Annotate Movie struct with json tag to customize the keys

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,11 +3,18 @@ package data
 import "time"
 
 type Movie struct {
-	ID        int64     // Unique integer ID for movie
-	CreatedAt time.Time // Timestamp for when the movie is added to the database
-	Title     string    // Movie title
-	Year      int32     // Movie release year
-	Runtime   int32     // Movie runtime (in minutes)
-	Genres    []string  // Slice of genres for the movies (romance, comedy, etc.)
-	Version   int32     // Version number starts with 1 and will be incremented each time movie info is updated
+	// Unique integer ID for movie.
+	ID int64 `json:"id"`
+	// Timestamp for when the movie is added to the database.
+	CreatedAt time.Time `json:"created_at"`
+	// Movie title.
+	Title string `json:"title"`
+	// Movie release year.
+	Year int32 `json:"year"`
+	// Movie runtime (in minutes).
+	Runtime int32 `json:"runtime"`
+	// Slice of genres for the movies (romance, comedy, etc.).
+	Genres []string `json:"genres"`
+	// Version number starts with 1 and will be incremented each time movie info is updated.
+	Version int32 `json:"version"`
 }
