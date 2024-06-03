eca50017e6d5fcb0e46e05388bbbaf5eaf13c2ab
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-03 12:01:48 +0100

    Update some struct tags for Movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -6,15 +6,15 @@ type Movie struct {
 	// Unique integer ID for movie.
 	ID int64 `json:"id"`
 	// Timestamp for when the movie is added to the database.
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"-"`
 	// Movie title.
 	Title string `json:"title"`
 	// Movie release year.
-	Year int32 `json:"year"`
+	Year int32 `json:"year,omitempty"`
 	// Movie runtime (in minutes).
-	Runtime int32 `json:"runtime"`
+	Runtime int32 `json:"runtime,omitempty"`
 	// Slice of genres for the movies (romance, comedy, etc.).
-	Genres []string `json:"genres"`
+	Genres []string `json:"genres,omitempty"`
 	// Version number starts with 1 and will be incremented each time movie info is updated.
 	Version int32 `json:"version"`
 }
