20c8b8f9e2a8eda2ee735d391cbf98c43079aaca
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-03 18:15:48 +0100

    Update notFoundResponse helper method header

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -32,7 +32,7 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 
 // notFoundResponse uses the errorResponse helper method to send 404 Not Found status code
 // and JSON response to the client.
-func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 	message := http.StatusText(http.StatusNotFound)
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
