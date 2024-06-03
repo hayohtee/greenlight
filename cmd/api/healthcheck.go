8b1c90520249a05994056e01dd9a2c047b19c81f
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-03 12:25:34 +0100

    Update the healthcheckHandler to use the envelope type

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,12 +6,15 @@ import (
 
 // healthcheckHandler write application information as json response
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Print(err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
