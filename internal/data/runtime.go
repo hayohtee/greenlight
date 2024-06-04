4bea0b57b1fe561d3626278cd2b5fc9bfd16366f
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-04 13:50:37 +0100

    Update runtime.go
    - Change the underlying type of Runtime to int32.
    - Change the MarshalJSON method to pointer receiver.
    - Add UnmarshalJSON method

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,17 +1,43 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 // Runtime A custom type which has underlying type int32
-type Runtime int64
+type Runtime int32
+
+// ErrInvalidRuntimeFormat is an error that indicates there is a problem
+// decoding the Runtime JSON value.
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 // MarshalJSON satisfies the json.Marshaler interface and return the
 // JSON-encoded value for the movie runtime ("<runtime> mins").
-func (r Runtime) MarshalJSON() ([]byte, error) {
+func (r *Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+// UnmarshalJSON satisfies the json.Unmarshaler interface and
+// decode JSON value for the field Runtime or throw ErrInvalidRuntimeFormat
+// if there is an error decoding the JSON value.
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[0] == "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	*r = Runtime(i)
+	return nil
 }
