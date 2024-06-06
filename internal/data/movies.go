bacc85b1b2a1cf3b6472f984af3fcedef3fb5176
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-06 17:33:56 +0100

    Write the code for Update method

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -94,7 +94,14 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 
 // Update specific record in the movie database.
 func (m MovieModel) Update(movie *Movie) error {
-	return nil
+	query := `
+		UPDATE movies
+		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		WHERE id = $5
+		RETURNING version;`
+
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID}
+	return m.DB.QueryRow(query, args).Scan(&movie.Version)
 }
 
 // Delete a specific movie from the database or return an error.
