93ecbbcd1e913e63b654727935a77709bb3fd68e
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-08 18:49:45 +0100

    Add new helper method for getting the sort column and the direction

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,9 @@
 package data
 
-import "github.com/hayohtee/greenlight/internal/validator"
+import (
+	"github.com/hayohtee/greenlight/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page         int
@@ -15,4 +18,25 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
+
+// sortColumn check that the client-provided Sort field matches one of the entries in
+// our safeList and if it does, extract the column name from the Sort field by stripping
+// the leading hyphen character (if one exists).
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+// sortDirection return the sort direction ("ASC" or "DESC") on the prefix character
+// of the Sort field.
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
