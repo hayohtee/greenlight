fa6da53568d983d0e53dea45a62fae463eae9778
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-08 18:55:43 +0100

    Update the query statement to include sort parameter in GetAll method

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"github.com/hayohtee/greenlight/internal/validator"
 	"github.com/lib/pq"
 	"time"
@@ -161,12 +162,12 @@ func (m MovieModel) Delete(id int64) error {
 
 // GetAll returns list of all movies in the database matching the query parameters.
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `
+	query := fmt.Sprintf(`
 		SELECT id, created_at, title, year, runtime, genres, version
 		FROM movies
 		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
 		AND (genres @> $2 OR $2 = '{}')
-		ORDER BY id;`
+		ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
