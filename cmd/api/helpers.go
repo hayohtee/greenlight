50a405fe4e9f624d8db95a93f6df57eef084f040
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-08 13:50:30 +0100

    Add new helper methods

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/hayohtee/greenlight/internal/validator"
 	"github.com/julienschmidt/httprouter"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 )
@@ -91,4 +93,42 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 		return errors.New("body must only contain a single JSON value")
 	}
 	return nil
+}
+
+// readString returns a string value from the query string, or the provided default
+// value if no matching key could be found.
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	return s
+}
+
+// readCSV read a string value from the query string and then splits it into slice
+// on the comma character. If no matching key could be found, it returns the provided
+// return value.
+func (app *application) readCSV(qs url.Values, key string, defaultValue []string) []string {
+	csv := qs.Get(key)
+	if csv == "" {
+		return defaultValue
+	}
+	return strings.Split(csv, ",")
+}
+
+// readInt reads a string value from the query string and converts it to an integer
+// before returning. If no matching key could be found, it returns the provided
+// return value. If the value could not be converted to an integer, then we record
+// an error message in the provided validator instance.
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer")
+		return defaultValue
+	}
+	return i
 }
