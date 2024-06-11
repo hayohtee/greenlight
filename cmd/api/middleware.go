cfcc49aa25c58c344d5d927396abcd311fc4f767
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-11 09:26:45 +0100

    Change the rate limit from setting global limit into each user.

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"golang.org/x/time/rate"
+	"net"
 	"net/http"
+	"sync"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -19,15 +21,32 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// Initialize a new rate limiter which allows an average of 2 requests per second,
-	// with a maximum of 4 requests in a single 'burst'.
-	limiter := rate.NewLimiter(2, 4)
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*rate.Limiter)
+	)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			// Initialize a new rate limiter which allows an average of 2 requests per second,
+			// with a maximum of 4 requests in a single 'burst'.
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
+		if !clients[ip].Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		mu.Unlock()
 		next.ServeHTTP(w, r)
 	})
 }
