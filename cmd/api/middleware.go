9ffd05ea8fe5146b0e3ab640e6dd4a84755e4cf0
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-11 09:36:45 +0100

    Add a mechanism for removing the clients that have not made request since 3 minutes

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/http"
 	"sync"
+	"time"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -21,10 +22,33 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
+	// Define a client struct to hold the rate limiter and last seen time for
+	// each client.
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
 	var (
 		mu      sync.Mutex
-		clients = make(map[string]*rate.Limiter)
+		clients = make(map[string]*client)
 	)
+
+	// Launch a background goroutine which removes old entries from the clients map
+	// once every minute.
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ip, _, err := net.SplitHostPort(r.RemoteAddr)
@@ -37,10 +61,15 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 		if _, found := clients[ip]; !found {
 			// Initialize a new rate limiter which allows an average of 2 requests per second,
 			// with a maximum of 4 requests in a single 'burst'.
-			clients[ip] = rate.NewLimiter(2, 4)
+			clients[ip] = &client{
+				limiter: rate.NewLimiter(2, 4),
+			}
 		}
 
-		if !clients[ip].Allow() {
+		// Update the last seen for the client.
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
 			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
