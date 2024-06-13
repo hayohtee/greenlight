ceda870311091099ee6d5a343ff97747b39f4644
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-13 15:27:09 +0100

    Add background helper method

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -132,3 +132,16 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 	}
 	return i
 }
+
+// background accepts an arbitrary function as a parameter launch the function
+// in a new goroutine and handle panic.
+func (app *application) background(fn func()) {
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				app.logger.PrintError(fmt.Errorf("%s", err), nil)
+			}
+		}()
+		fn()
+	}()
+}
