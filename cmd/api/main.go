14b916ffe7a758ac3ac73fee5fbf8ccd2b2b62d9
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-20 09:03:33 +0100

    Change the default value of db-dsn commandline flag

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -23,7 +23,7 @@ func main() {
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 
 	// Reads the DSN value from db-dsn command-line flag into the config struct.
-	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "PostgreSQL DSN")
 
 	// Reads the connection pool settings from the command-line flags into the config struct
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
